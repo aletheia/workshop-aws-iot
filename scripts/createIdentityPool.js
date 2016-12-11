5b8c90c2392e31e4aa673189b6bad582fb680144
Luca Bianchi <bianchi.luca@gmail.com>
2016-12-11 16:07:36 +0100

    added function to handle cognito requests added cognito custom roles and policies to serverless template addede sample test events for create and delete

---
+++ scripts/createIdentityPool.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const https = require('https');
+const url = require('url');
+const AWS = require('aws-sdk');
+const cognitoidentity = new AWS.CognitoIdentity();
+const SUCCESS = 'SUCCESS';
+const FAILED = 'FAILED';
+
+const UNKNOWN = {
+	Error: 'Unknown operation'
+};
+const requestTypes = [
+	'Create',
+	'Update',
+	'Delete'
+];
+
+const handleIdentityPoolOperation = (event, context, callback) => {
+
+    function respond(responseStatus, responseData, physicalResourceId) {
+        const responseBody = JSON.stringify({
+            Status: responseStatus,
+            Reason: `See the details in CloudWatch Log Stream: ${context.logStreamName}`,
+            PhysicalResourceId: physicalResourceId || context.logStreamName,
+            StackId: event.StackId,
+            RequestId: event.RequestId,
+            LogicalResourceId: event.LogicalResourceId,
+            Data: responseData
+        });
+
+        console.log('Response body:\n', responseBody);
+
+        const parsedUrl = url.parse(event.ResponseURL);
+        const options = {
+            hostname: parsedUrl.hostname,
+            port: 443,
+            path: parsedUrl.path,
+            method: 'PUT',
+            headers: {
+                'content-type': '',
+                'content-length': responseBody.length
+            }
+        };
+
+        return new Promise((resolve, reject) => {
+                const request = https
+                    .request(options, resolve);
+
+                request.on('error', error => reject(`send(..) failed executing https.request(..): ${error}`));
+                request.write(responseBody);
+                request.end();
+            })
+            .then(() => callback(responseStatus === FAILED ? responseStatus : null, responseData))
+            .catch(callback);
+    }
+
+    function createIdentityPool(poolId,iamRole) {
+        var params = {
+            AllowUnauthenticatedIdentities: true,
+            IdentityPoolName: poolId
+        };
+        cognitoidentity.createIdentityPool(params).promise()
+		    .then(data => {
+		        var setRoleParams ={
+		            IdentityPoolId:data.IdentityPoolId,
+		            Roles: {
+		                authenticated:iamRole,
+		                unauthenticated:iamRole
+		            }
+		        }
+		        return cognitoidentity.setIdentityPoolRoles(setRoleParams).promise()
+		    })
+		    .then(data => respond(SUCCESS, data, data.IdentityPoolId))
+		    .catch(error => respond(FAILED, {message: error}));
+    }
+
+    function deleteIdentityPool(poolId){
+      var params = {
+          IdentityPoolId: poolId
+      };
+      cognitoidentity.deleteIdentityPool(params).promise()
+      .then(data => respond(SUCCESS, data, data.IdentityPoolId))
+      .catch(error => respond(FAILED, {message: error}));
+    }
+
+
+    console.log(`Performing operation ${event.RequestType} on Cognito Identity Pool ${event.LogicalResourceId}`);
+
+    switch(event.RequestType) {
+        case 'Create':
+            createIdentityPool(event.LogicalResourceId,event.ResourceProperties.IAMRole);
+        break;
+        case 'Delete':
+            deleteIdentityPool(event.PhysicalResourceId);
+        break;
+        case 'Update':
+            createIdentityPool(event.LogicalResourceId,event.ResourceProperties.IAMRole);
+            deleteIdentityPool(event.PhysicalResourceId);
+        break;
+        default:
+            callback('Something went wrong');
+  }
+}
+
+
+
+exports.handler = handleIdentityPoolOperation;
